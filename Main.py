e22ef67ede5053be34392a11ab3ae4e69a983e7b
Learn 2 Code <90368292+Learn2Code010@users.noreply.github.com>
2021-09-09 09:15:18 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,30 @@
+# Lets Start
+
+# 1st step : we need to take the input from user
+
+input = str(input("Enter A Phrase :"))
+
+# 2nd step : now we need to split the phrase / sentence
+
+text = input.split()
+acronym = " "
+
+# now let's start the for loop
+
+for i in text:  # (:) this symbol is called as indentation
+    acronym = acronym + str(i[0]).upper()
+
+    # here the upper() function is used to get the Upper Case Letters that we give
+
+# now let's print the acronym
+
+print(acronym)
+
+# now let's run the code and check
+
+# so now its asking to enter a phrase lets enter it  , so I have given it Machine Learning so we will get
+# output as ML , LET'S SEE , So YESS!! We got it
+
+  # THANKS FOR WATCHING DO SHARE , SUBCRIBE AND LIKE AND TRY OUT THIS PROJECT YOURSELVES ONCE
+
+  # HAPPY LEARNING !!
